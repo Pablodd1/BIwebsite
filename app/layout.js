11739a0a9229c3a0d0e7016c15330bef83d3fe24
Pablodd1 <148375533+Pablodd1@users.noreply.github.com>
2026-05-27 16:13:27 -0400

    feat: add Google Tag Manager snippets

--- app/layout.js
+++ app/layout.js
@@ -128,6 +128,16 @@ export default async function RootLayout({ children }) {
   return (
     <html lang={lang} suppressHydrationWarning>
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-K6KPJCJ6');`,
+          }}
+        />
         {/* Google tag (gtag.js) */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=AW-18156507743" />
         <script
@@ -249,6 +259,15 @@ export default async function RootLayout({ children }) {
         />
       </head>
       <body className={`${montserrat.className} font-medium`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-K6KPJCJ6"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
         <RootLayoutClient lang={lang} dict={dict}>
           {children}
         </RootLayoutClient>
